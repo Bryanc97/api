0bb3342f3f78027df1514f89f30331735314ec7a
Bryan Cuero <121645713+Bryanc97@users.noreply.github.com>
2023-12-28 23:59:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors({
-    origin: 'https://shyest-economies.000webhostapp.com',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-}));
 
 
 const pool = new pg.Pool({
